a24374218e7ad1deeb7312ee2da5f9d74679debd
Fisixus <31778258+Fisixus@users.noreply.github.com>
2020-01-17 18:10:29 +0300

    Map has been calculated and created.

--- GameManager.cpp
+++ GameManager.cpp
@@ -56,6 +56,27 @@ typedef struct {
 	Color color;
 } Coin;
 
+
+void initFunc(void)
+{
+
+	glViewport(0, 0, ww, wh);
+
+	/* Pick 2D clipping window to match size of screen window
+	This choice avoids having to scale object coordinates
+	each time window is resized */
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, (GLdouble)ww, 0.0, (GLdouble)wh);
+
+	/* set clear color to black and clear window */
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+
+}
+
 /* rehaping routine called whenever window is resized
 or moved */
 
@@ -78,26 +99,7 @@ void reshapeFunc(GLsizei w, GLsizei h)
 
 	ww = w;
 	wh = h;
-}
-
-void initFunc(void)
-{
-
-	glViewport(0, 0, ww, wh);
-
-	/* Pick 2D clipping window to match size of screen window
-	This choice avoids having to scale object coordinates
-	each time window is resized */
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, (GLdouble)ww, 0.0, (GLdouble)wh);
-
-	/* set clear color to black and clear window */
-
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-
-
+	glutPostRedisplay();
 }
 
 void keyboardFunc(unsigned char key, int x, int y)
@@ -106,6 +108,12 @@ void keyboardFunc(unsigned char key, int x, int y)
 	{
 		exit(0);
 	}
+}
+
+// Callback routine for non-ASCII key entry.
+void specialKeyInputFunc(int key, int x, int y)
+{
+
 	if (key == GLUT_KEY_UP)
 	{
 		//TODO
@@ -123,6 +131,7 @@ void keyboardFunc(unsigned char key, int x, int y)
 		//TODO
 	}
 
+	glutPostRedisplay();
 }
 
 
@@ -140,14 +149,94 @@ void mouseFunc(int btn, int state, int x, int y)
 	}
 }
 
+void drawRoads(int sideWalkNumber, int topSidewalky) 
+{
+	int topRoady = topSidewalky;
+	int totalNumberOfTape = (ww / (ww / 50 + ww / 100)) + 1;
+	if(sideWalkNumber == 1 || sideWalkNumber == 3 || sideWalkNumber == 5 )
+	{
+		for (int i = 0; i < 3; i++) 
+		{
+			int leftTapeofRoadx = 0;
+			int rightTapeofRoadx = ww / 50;
+			topRoady += wh / 24;
+			for (int j = 0; j < totalNumberOfTape; j++)
+			{
+				glBegin(GL_LINES);
+					glVertex2f(leftTapeofRoadx, topRoady);
+					glVertex2f(rightTapeofRoadx, topRoady);
+				glEnd();
+				leftTapeofRoadx = rightTapeofRoadx + ww / 100;
+				rightTapeofRoadx = leftTapeofRoadx + ww / 50;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			int leftTapeofRoadx = 0;
+			int rightTapeofRoadx = ww / 50;
+			topRoady += wh / 24;
+			for (int j = 0; j < totalNumberOfTape; j++)
+			{
+				glBegin(GL_LINES);
+				glVertex2f(leftTapeofRoadx, topRoady);
+				glVertex2f(rightTapeofRoadx, topRoady);
+				glEnd();
+				leftTapeofRoadx = rightTapeofRoadx + ww / 100;
+				rightTapeofRoadx = leftTapeofRoadx + ww / 50;
+			}
+		}
+	}
+}
+
+void drawSideWalks(int bottomSidewalky, int topSidewalky)
+{	
+	glBegin(GL_TRIANGLE_STRIP);
+		glVertex2f(0, bottomSidewalky);
+		glVertex2f(0, topSidewalky);
+		glVertex2f(ww, bottomSidewalky);
+		glVertex2f(ww, topSidewalky);
+	glEnd();
+}
+
+void drawMap() 
+{
+	int bottomSidewalky, topSidewalky, totalRoadLengthy;
+	totalRoadLengthy = wh - wh / 4;
+	bottomSidewalky = 0;
+	topSidewalky = wh / 24;
+	for (int i = 1; i <= 6; i++)
+	{
+		drawSideWalks(bottomSidewalky, topSidewalky);
+		drawRoads(i, topSidewalky);
+
+		if (i==1 || i == 3 || i == 5)
+		{
+			bottomSidewalky = topSidewalky + (4*totalRoadLengthy) / 18;
+			topSidewalky = bottomSidewalky + wh / 24;
+		}
+		else
+		{
+			bottomSidewalky = topSidewalky + (3*totalRoadLengthy) / 18;
+			topSidewalky = bottomSidewalky + wh / 24;
+		}
+
+	}
+}
+
 /* display callback required by GLUT 3.0 */
 
 void displayFunc(void)
 {
-	int i, x, y;
+	
 	glClear(GL_COLOR_BUFFER_BIT);
-
+	//Sidewalks
+	drawMap();
+	
 	glFlush();
+	glutSwapBuffers();
 }
 
 int main(int argc, char** argv)
@@ -162,6 +251,7 @@ int main(int argc, char** argv)
 	glutDisplayFunc(displayFunc);
 	//glutTimerFunc(20, myTimeout, 0);
 	glutKeyboardFunc(keyboardFunc);
+	glutSpecialFunc(specialKeyInputFunc);
 
 	glutMainLoop();
 
